4db4a0549f7c598fce1f8d7379d24d74ee35a7d7
RonnieHoffer <ronnie.hoffer@gmail.com>
2020-12-06 22:01:55 -0500

    Final commit for Exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,35 @@
-puts "What is your name?"
-user_name = gets
-puts "Hello, #{user_name}"
+# puts "What is your name?"
+# user_name = gets
+# puts "Hello, #{user_name}"
+
+# puts "\n"
+
+# puts "How many years old are you?"
+# my_age = gets.to_i
+# puts "You are #{my_age} years old."
+
+# number = gets.to_i
+# if number > 0
+#     puts "#{number} is positive"
+# else
+#     puts "#{number} is negative"
+# end
+
+x = gets.to_i
+y = gets.to_i
+
+if x > y
+    puts "x is greater than y!"
+elsif x < y
+    puts "x is less than y!"
+else
+    puts "x equals y!"
+end
+
+if x != 10
+    puts "I get printed!"
+end
+
+unless x == 10
+    puts "I get printed a second time."
+end
