0fe2a6d1b72f693251f78ddccdffb05165d04210
RonnieHoffer <ronnie.hoffer@gmail.com>
2020-12-08 19:48:06 -0500

    Assignment 5 completed

--- exercise5.rb
+++ exercise5.rb
@@ -1,23 +1,39 @@
-puts "Hello, there.  I see you are about to get some exercise outside."
 distance = 0
-energy = 3
+energy = 2
+puts "Hello, there.  I see you are about to get some exercise outside."
+puts "Your current energy level is #{energy}."
 
 while distance < 42
     puts "Will you be walking (w) or running (r), or are you ready to go home (g)?"
     exercise_mode = gets.chomp
+
     
     if exercise_mode == "w"
         distance += 1
+        energy += 1
+    
     elsif exercise_mode == "r"
-        distance += 5
+        if energy > 0
+            distance += 5
+            energy -= 1
+        else
+            puts "You do not have enough energy to run!  Try walking to regain some steam."
+        end
+    
     elsif exercise_mode == "g"
-        puts "Great job today.  Have a great day."
+        puts "\n"
+        puts "Great job today.  Your current energy level is #{energy}.  Have a great day."
         break
+    
     else
         puts "Invalid entry.  Try again."
+    
     end
 
-    puts "You are currently #{distance}km from home."
+
+    puts "\n"
+    puts "You are now #{distance}km from home."
+    puts "Your current energy level is #{energy}."
 end
 
 if distance >= 42
